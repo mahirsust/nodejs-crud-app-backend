2d5cc740a1c73818b6b4f12c2b21c075ce612575
mahirsust <mahirhasancse@gmail.com>
2024-09-15 20:25:16 +0200

    Update feat: change NodeJS enviroment PORT setup

--- index.js
+++ index.js
@@ -25,15 +25,17 @@ app.get("/", (req, res) => {
 });
 
 // db and server connect
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PROD_PORT : process.env.DEV_PORT;
 const dbConnectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@backenddb.tihcy.mongodb.net/${process.env.DB_COLLECTION_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`
+
 mongoose
   .connect(
     dbConnectionString
   )
   .then(() => {
     console.log("Connected to the Database!");
-    app.listen(process.env.DEV_PORT, () => {
-      console.log(`Server is running on port ${process.env.DEV_PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
     });
   })
   .catch(() => {
